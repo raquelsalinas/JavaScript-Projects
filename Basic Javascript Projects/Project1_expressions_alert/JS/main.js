d8bd93fb005c678b4a740ef798bfdc21202c2ad1
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-06-21 18:14:18 -0500

    Fixing files

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+//Establishing variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+//Utilizing  document.write method and concatination
+document.write(Sent1+Sent2);
+
+//Utilizing window.alert
+=======
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+document.write(Sent1+Sent2);
+
+>>>>>>> 69a59478f9cd5b7f95f0361fd8400a3436f07c78
+window.alert("Hello, World!");
