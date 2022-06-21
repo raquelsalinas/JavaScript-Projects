a2fd50fa6a6777c832cb751c7a0a8a9b7ec2f0a5
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-06-21 17:59:18 -0500

    Submitting assignments

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,9 @@
+//Establishing variables
 var Sent1 = "This is the beginning of the string";
 var Sent2 = " and this is the end of the string";
 
+//Utilizing  document.write method and concatination
 document.write(Sent1+Sent2);
 
+//Utilizing window.alert
 window.alert("Hello, World!");
