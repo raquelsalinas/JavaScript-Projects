fe51a12af4d8dbea0260143e308cf88425fac11c
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-06-21 18:31:43 -0500

    Merge branch 'main' of https://github.com/raquelsalinas/JavaScript-Projects

--- Basic Javascript Projects/Project2_functions/JS/main.js
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -1,4 +1,6 @@
+//Stating my first function
 function My_First_Function() {
-    var str = "This is the button text!";
+    var str = "This is the button text,";
+    str += " and this is more text.";
     document.getElementById("Button_Text").innerHTML = str;
 }
