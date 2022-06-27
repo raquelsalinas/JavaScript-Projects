db3f9996145cbd132477f7463d47db1368046096
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-06-27 18:04:34 -0500

    Submitting assignment

--- Basic Javascript Projects/Project10_loops_arrays/JS/main.js
+++ Basic Javascript Projects/Project10_loops_arrays/JS/main.js
@@ -1,3 +1,15 @@
+//While loop
+function count_To_Ten() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+}
+
+//For loop
 var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
 var Content = "";
 var Y;
@@ -8,6 +20,7 @@ var Y;
         document.getElementById("List_of_Instruments").innerHTML = Content;
     }
 
+//Array
 function cat_pics() {
     var Cat_Picture = [];
     Cat_Picture[0] = "sleeping";
@@ -21,5 +34,25 @@ function constant_function() {
     const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
     Musical_Instrument.color = "blue";
     Musical_Instrument.price = "$900";
-    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price;
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price;    
 }
+
+//Let keyword
+var X = 82;
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description: function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
