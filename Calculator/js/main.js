068bf302effd2b6bd0cb4a4dd8f4143a3c392ca7
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-06-30 21:21:29 -0500

    Submitting assignment

---
+++ Calculator/js/main.js
@@ -0,0 +1,103 @@
+//Creates an object to keep track of values
+const Calculator = {
+    //This will display 0 on calculator screen
+    Display_Value: '0',
+    //This will hold first operand for any expression
+    First_Operand: null,
+    //This checks whether or not second operand has been inputted
+    Wait_Second_Operand: false,
+    //This will hold operator
+    operator: null,
+};
+
+//Modifies values each time a button is clicked on
+function Input_Digit(digit) {
+    const {Display_Value, Wait_Second_Operand } = Calculator;
+    //Checks if Wait_Second_Operand is true and sets Display_Value to key that was clicked on
+    if (Wait_Second_Operand === true) {
+        Calculator.Display_Value = digit;
+        Calculator.Wait_Second_Operand = false;
+    } else {
+        //Overwrites Display_Value if the current value is 0 otherwise it adds onto it
+        Calculator.Display_Value = Display_Value ==='0' ? digit : Display_Value + digit;
+    }
+}
+
+//This section handles decimal points
+function Input_Decimal(dot) {
+    //Ensures that accidental clicking of the decimal point doesn't cause bugs in operation
+    if (Calculator.Wait_Second_Operand === true) return;
+    if (!Calculator.Display_Value.includes(dot)) {
+        //Saying that if the Display_Value doesn't contain a decimal point we want to add a decimal point
+        Calculator.Display_Value += dot;
+    }
+}
+
+//This sections handles operators
+function Handle_Operator(Next_Operator) {
+    const {First_Operand, Display_Value, operator} = Calculator;
+    //When an operator key is pressed we convert the current number displayed on the screen to a number and then store
+    //the result in Calculator.First_Operand if it doesn't already exist
+    const Value_of_Input = parseFloat(Display_Value);
+    //Checks if an operator already exists and if Wait_Second_Operand is true, then updates the operator an exit from function
+    if (operator && Calculator.Wait_Second_Operand) {
+        Calculator.First_Operand = Value_of_Input;
+    } else if (operator) {
+        const Value_Now = First_Operand || 0;
+        let result = Perform_Calculation[operator](Value_Now, Value_of_Input);
+        result = Number(result).toFixed (9);
+        result = (result *1).toString();
+        Calculator.Display_Value = parseFloat(result);
+        Calculator.First_Operand = parseFloat(result);
+    }
+    Calculator.Wait_Second_Operand = true;
+    Calculator.operator = Next_Operator;
+}
+const Perform_Calculation = {
+    '/': (First_Operand, Second_Operand) => First_Operand / Second_Operand,
+    '*': (First_Operand, Second_Operand) => First_Operand * Second_Operand,
+    '+': (First_Operand, Second_Operand) => First_Operand + Second_Operand,
+    '-': (First_Operand, Second_Operand) => First_Operand - Second_Operand,
+    '=': (First_Operand, Second_Operand) => Second_Operand,
+};
+
+function Calculator_Reset() {
+    Calculator.Display_Value = '0';
+    Calculator.First_Operand = null;
+    Calculator.Wait_Second_Operand = false;
+    Calculator.operator = null;
+}
+
+//Function updates calculator screen with contents of Display_Value
+function Update_Display() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = Calculator.Display_Value;
+}
+
+Update_Display();
+//Section monitors button clicks
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click'), (event) => {
+    const {target} = event;
+    if (!target.matches('button')) {
+        return;
+    }
+    if (target.classList.contains('operator')) {
+        Handle_Operator(target.value);
+        Update_Display();
+        return;
+    }
+    if (target.classList.contains('decimal')) {
+        Input_Decimal(target.value);
+        Update_Display();
+        return;
+    }
+    //Ensures AC clears all inputs from screen
+    if (target.classList.contains('all-clear')) {
+        Calculator_Reset();
+        Update_Display();
+        return;
+    }
+    Input_Digit(target.value);
+    Update_Display();
+}
